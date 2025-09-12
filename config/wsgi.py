2e581380b7629c0c6b48d7ff8fe6fdb87a274305
kislayraj-ai <kislayrajai06@gmail.com>
2025-09-12 12:57:29 +0530

    CONfigured the wsgi for whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+# Serve static files using Whitenoise
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), '../staticfiles'), prefix='static/')
